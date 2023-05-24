addae8992c8b2c8453da92933eeed24dda0d1182
marianolrc <marianoagonzalez95@gmail.com>
2023-05-24 15:50:11 -0300

    Funcionalidad de Cerrar sesion agregada

---
+++ js/cerrarSesion.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const logoutLink = document.getElementById('sesion-tab');
+  
+    logoutLink.addEventListener('click', function(e) {
+      e.preventDefault(); // Evitar que se siga el enlace
+      
+      // Eliminar el estado de inicio de sesión del localStorage
+      localStorage.removeItem('isLoggedIn');
+      
+      // Redirigir a la página de inicio de sesión
+      window.location.href = '../index.html'; // Reemplaza 'login.html' con la URL de la página de inicio de sesión
+    });
+  });
+  
